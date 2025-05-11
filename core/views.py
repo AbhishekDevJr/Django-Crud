50b6663089e8e4cc4c10c689827a23a82f326512
Abhishek Choudhari <abhishek007coc@gmail.com>
2025-05-11 22:53:07 +0530

    Project List API developed

--- core/views.py
+++ core/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from . models import Project
+from . serializers import ProjectListSerializer
 
-# Create your views here.
+class ProjectListView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        try:
+            project_data = Project.objects.all()
+            
+            if not project_data:
+                return Response({
+                    'status': 'success',
+                    'message': 'Project data not found',
+                    'data': []
+                }, status=status.HTTP_200_OK)
+                
+            serialized_data = ProjectListSerializer(project_data, many=True)
+            return Response({
+                'status': 'success',
+                'message': 'Project Data Found',
+                'data': serialized_data.data
+            })
+            
+        except Exception as e:
+            return Response({
+                'status': 'error',
+                'message': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
